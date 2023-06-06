3c7d7afc62bc5a4c6d6922e31b57190c333b825c
超级帅的张总 <978524088@qq.com>
2023-06-06 20:15:59 +0800

    更新 vscode调试环境，并且将mian 中使用字符串类型修改。print和println区别测试

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+
+/*
+ * @Author       : Zry && 978524088@qq.com
+ * @Date         : 2023-06-01 22:10:36
+ * @LastEditors  : Zry && 978524088@qq.com
+ * @LastEditTime : 2023-06-06 19:36:28
+ * @FilePath     : \greeting\src\main.rs
+ * @Description  : 
+ * 
+ * Copyright (c) 2023 by 978524088@qq.com, All Rights Reserved. 
+ */
+
 fn main() {
-    println!("Hello, world!");
+    let name  = String::from("Zry") ;
+    println!("Hello {0}, {0}’s world!",name);
 }
